8b2e8174f0714f688457768884dfc2d9b7b701a6
Daniel Schreck <daniel.s.schreck@gmail.com>
2021-01-02 18:53:52 -0500

    cleaning up prompt

--- colors/syntax-highlighting.zsh
+++ colors/syntax-highlighting.zsh
@@ -1,22 +1,7 @@
 #! /bin/zsh
 
-# color palette for onedark-palenight custom highlighting
-blue=39
-cyan=81
-green=114
-ice=153
-orange=173
-pink=177
-purpink=135
-purple=99
-red=204
-softyellow=223
-tblue=26 # terminal default blue
-testval=09
-yellow=221
-pnpurple=104
-pnpurpblue=69
-pnhighlight=60  # 04 is good too
+# get palette colors
+source $dotfiles/colors/palenight_palette.sh
 
 # zsh-syntax-highlighting custom array
 : ${ZSH_HIGHLIGHT_STYLES[arg0]:=fg=$green,bold}
