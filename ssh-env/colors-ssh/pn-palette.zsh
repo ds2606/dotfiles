ecd58e0bb6514812007f95d68e3fd41070e1b8d3
Daniel Schreck <daniel.s.schreck@gmail.com>
2021-01-10 20:53:35 -0500

    created portable ssh environment for quick setup on any new machine

---
+++ ssh-env/colors-ssh/pn-palette.zsh
@@ -0,0 +1,50 @@
+# color palette for onedark-palenight custom highlighting
+pn_colors () {
+    if [[ $1 == '256' ]]; then
+        declare -xA c=(
+            blue 81
+            cyan 14
+            darkpurp 63
+            green 114  # 120 is vivid
+            ice 153  # 159 is vivid
+            orange 209
+            pink 213
+            pnhighlight 104  # 04 is good too
+            pnpurpblue 111
+            pnpurple 147
+            pnyellow 214
+            purpink 177
+            purple 141
+            red 211
+            softyellow 223
+            tblue 69  # terminal default blue
+            testval 09
+            yellow 221
+        )
+    elif [[ $1 == 'gui' ]]; then
+        declare -xA c=(
+            blue '#5fd7ff'
+            cyan '#00ffff'
+            darkpurp '#5f5fff'
+            green '#87d787'
+            ice '#afd7ff'
+            orange '#ff875f'
+            pink '#ff87ff'
+            pnhighlight '#8787d7'  # 04 is good too
+            pnpurpblue '#87afff'
+            pnpurple '#afafff'
+            pnyellow '#ffaf00'
+            purpink '#d787ff'
+            purple '#af87ff'
+            red '#ff87af'
+            softyellow '#d7af87'
+            tblue '#5f87ff'  # terminal default blue
+            testval '#ff0000'
+            yellow '#ffd75f'
+        )
+    elif [[ $1 == '16' ]]; then
+        echo '16 colors not yet implemented'
+    else
+        echo 'usage: pn_colors [256 | gui | 16]'
+    fi
+}
---
+++ ssh-env/colors-ssh/pn-syntax.zsh
@@ -0,0 +1,37 @@
+#! /bin/zsh
+
+# get palette colors
+source ~/ssh-env/colors-ssh/pn-palette.zsh && pn_colors 256
+
+# zsh-syntax-highlighting custom array
+: ${ZSH_HIGHLIGHT_STYLES[arg0]:=fg=$c[green],bold}
+: ${ZSH_HIGHLIGHT_STYLES[assign]:=fg=$c[purpink],bold}
+: ${ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]:=fg=$c[ice],bold}
+: ${ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]:=fg=$c[ice],bold}
+: ${ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]:=fg=$c[ice],bold}
+: ${ZSH_HIGHLIGHT_STYLES[back-quoted-argument]:=fg=$c[ice],bold}
+: ${ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]:=fg=$c[red],bold}
+: ${ZSH_HIGHLIGHT_STYLES[command-substitution]:=fg=$c[pnpurpblue]}
+: ${ZSH_HIGHLIGHT_STYLES[commandseparator]:=fg=$c[ice],bold}
+: ${ZSH_HIGHLIGHT_STYLES[comment]:=fg=$c[ice],bold}
+: ${ZSH_HIGHLIGHT_STYLES[default]:=none}
+: ${ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]:=fg=bold}
+: ${ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]:=fg=bold}
+: ${ZSH_HIGHLIGHT_STYLES[double-quoted-argument]:=fg=$c[green]}
+: ${ZSH_HIGHLIGHT_STYLES[double-hyphen-option]:=fg=$c[softyellow],bold}
+: ${ZSH_HIGHLIGHT_STYLES[globbing]:=fg=$c[ice]}
+: ${ZSH_HIGHLIGHT_STYLES[history-expansion]:=fg=$c[red],bold}
+: ${ZSH_HIGHLIGHT_STYLES[named-fd]:=fg=$c[testval]}
+: ${ZSH_HIGHLIGHT_STYLES[path]:=fg=$c[pnpurple],bold}
+: ${ZSH_HIGHLIGHT_STYLES[path_pathseparator]:=}
+: ${ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]:=}
+: ${ZSH_HIGHLIGHT_STYLES[precommand]:=fg=$c[orange]}
+: ${ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]:=fg=$c[red],bold}
+: ${ZSH_HIGHLIGHT_STYLES[process-substitution]:=fg=$c[pnpurpblue]}
+: ${ZSH_HIGHLIGHT_STYLES[rc-quote]:=fg=$c[testval]}
+: ${ZSH_HIGHLIGHT_STYLES[redirection]:=fg=$c[ice],bold}
+: ${ZSH_HIGHLIGHT_STYLES[reserved-word]:=fg=$c[softyellow],bold}
+: ${ZSH_HIGHLIGHT_STYLES[single-hyphen-option]:=fg=$c[softyellow],bold}
+: ${ZSH_HIGHLIGHT_STYLES[single-quoted-argument]:=fg=$c[green]}
+: ${ZSH_HIGHLIGHT_STYLES[suffix-alias]:=fg=$c[testval]}
+: ${ZSH_HIGHLIGHT_STYLES[unknown-token]:=}
