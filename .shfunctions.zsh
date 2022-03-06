f3bd5edc1d89341421e4f3ac764d2e9cb40236d0
Daniel Schreck <daniel.s.schreck@gmail.com>
2022-03-06 14:14:33 -0500

    overhaul nested_tmux() function
    Many updates to nested_tmux() to improve functionality and reliability.
    Added ability to list sessions with '-l' flag. Added ability to reattach
    to target nested sesion with '-n <nested-session-number>'. And fixed up
    the reliability of creating new sessions by removing use of shell
    environment variables (which were unreliable between different zsh
    processes in differnt tmux clients).

--- .shfunctions.zsh
+++ .shfunctions.zsh
@@ -87,28 +87,33 @@ call() {
 ## could make invocation code paths for different flag/arg combinations much cleaner in
 ## general (e.g. 'tmux_nested a b c d e f g' code path should be cleanly handled)
 tmux_nested() {
+    if [[ $# -eq 0 ]]; then
+        # if invoked without flags, print usage string and exit
+        echo 'usage: tmux-nested <[-n] | [-a nested-session-number] | [-l]>'
+        return 1
+    fi
+
     # read flags and check for number of existing nested sessions
-    local nested_sessions n_nested session_name
-    nested_sessions=$(tmux list-sessions -F '#{session_name}' | grep '^nested' --color=never)
-    [[ -z $nested_sessions ]] && n_nested=0 || n_nested=$(wc -l <<< "$nested_sessions")
+    local nested_sessions session_name
+    nested_sessions=$(tmux list-sessions -F '#{session_name}' | grep -P '^nested' --color=never | sort -V)
     [[ $1 == "-n" ]] && local _new=1 && shift
     [[ $1 == "-a" ]] && local _attach=1 && shift
     [[ $1 == "-l" ]] && echo "$nested_sessions" && return 0
 
-    if [[ $# -eq 0 ]] && [[ _new -ne 1 ]]; then
-        # if invoked without flags, print usage string and exit
-        echo 'usage: tmux-nested <[-n] | [-a nested-session-number] | [-l]>'
-        return 1;
-    elif [[ $_new -eq 1 ]]; then
+    if [[ $_new -eq 1 ]]; then
         # if new session requested: create, set <C-b> tmux prefix, and attach
-        session_name="nested$((n_nested+1))"
-        env TMUX='' tmux new-session -d -As "$session_name"
+        for (( i = 1;; i++ )); do
+            # get lowest available numeric value for new session name
+            grep -Pq "\w$i$" <<< "$nested_sessions"  # debug? sometimes the '$' anchor seems to be failing for the herestring '\n' newline literals
+            [[ $? -eq 1 ]] && session_name="nested$i" && break
+        done
+        env TMUX='' tmux new-session -ds "$session_name"
         tmux send-keys -t "$session_name" "tmux set prefix C-b" ENTER "clear" ENTER
-        env TMUX='' tmux attach -t "$session_name"
+        env TMUX='' tmux attach -t "$session_name" || return
     # attach to specified session if requested
     elif [[ $_attach -eq 1 ]]; then
         session_name="nested$1"
-        env TMUX='' tmux attach -t "$session_name"
+        env TMUX='' tmux attach -t "$session_name" || return
     fi
 }
 
